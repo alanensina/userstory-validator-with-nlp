8e2c2dd107ced75eed6684c186ed60d658954e97
Alan Ensina <alanvinicius.ce@gmail.com>
2022-09-02 18:12:17 +0100

    Removido books

--- src/models/books.py
+++
@@ -1,7 +0,0 @@
-from flask_restx import fields
-from src.server.instance import server
-
-book = server.api.model('Book', {
-    'id': fields.Integer(description='Id do livro'),
-    'title': fields.String(required=True, description='Título do livro')
-})
