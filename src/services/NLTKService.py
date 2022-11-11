065a6b63720a03bfb54f7bda22a02bbb011c8772
Alan Ensina <alanvinicius.ce@gmail.com>
2022-11-11 18:09:27 +0000

    Duplicidade de código removida.

--- src/services/NLTKService.py
+++ src/services/NLTKService.py
@@ -1,9 +1,7 @@
-import nltk, joblib, timeit
+import nltk, joblib
 from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize, sent_tokenize
-from src.classes.ResponseHistoria import ResponseHistoria
-from src.classes.ResponseCenario import ResponseCenario
 from src.classes import Constantes
 from src.services.UtilsService import utils
 
@@ -12,94 +10,8 @@ nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('averaged_perceptron_tagger')
 
-class NLTKService():
-    
-    def processarHistoria(idioma:str, historia:str):
-        start = timeit.default_timer()
-        finalidade = None
-        erros = None
-
-        sentencas = utils.separar_sentencas_historia(historia)
-        
-        sentencas_processadas = []
-        
-        if len(sentencas) > 1 and not utils.verifica_erro_separacao(sentencas):
-            x = 0
-
-            while x < len(sentencas):
-                sentencas_processadas.append(NLTKService.processar(sentencas[x], idioma))
-                x = x + 1
-
-            tags = []
-            for sp in sentencas_processadas:
-                for s in sp:
-                    tags.append(s.retorna_estrutura())
-
-            bem_formada = utils.verifica_C1_historia(sentencas_processadas)
-            atomica = utils.verifica_C2_historia(sentencas_processadas)
-            minima = utils.verifica_C3_historia(sentencas_processadas, bem_formada)
-            ator = utils.extrair_ator(sentencas_processadas[0])    
-            acao = utils.extrair_acao(sentencas_processadas[1])
-
-            if len(sentencas) > 2:      
-                finalidade = utils.extrair_finalidade(sentencas_processadas[2])  
-                erros = utils.verifica_erros_historia(bem_formada, atomica, minima, utils.valida_ator_historia(sentencas_processadas[0]), utils.valida_acao_historia(sentencas_processadas[1]), utils.valida_finalidade_historia(sentencas_processadas[2]))
-            else:
-                erros = utils.verifica_erros_historia(bem_formada, atomica, minima, utils.valida_ator_historia(sentencas_processadas[0]), utils.valida_acao_historia(sentencas_processadas[1]), None)
-                
-            end = timeit.default_timer()  
-            tempo = utils.formatar_tempo(start, end)
-
-            if erros == None:
-                return ResponseHistoria(historia, Constantes.NLTK, tempo, bem_formada, atomica, minima, ator, acao, finalidade, tags, erros)
-            else:
-                return ResponseHistoria(historia, Constantes.NLTK, tempo, bem_formada, atomica, minima, None, None, None, tags, erros)
-        else:
-            end = timeit.default_timer()  
-            tempo = utils.formatar_tempo(start, end)
-            return utils.retorna_erro_historia(historia, Constantes.NLTK, sentencas, tempo)  
-           
-        
-    def processarCenario(idioma:str, cenario:str):
-        start = timeit.default_timer()
-
-        sentencas = utils.separar_sentencas_cenario(cenario)
-        
-        sentencas_processadas = []
-
-        if len(sentencas) > 1 and not utils.verifica_erro_separacao(sentencas):
-            x = 0
-
-            while x < len(sentencas):
-                sentencas_processadas.append(NLTKService.processar(sentencas[x], idioma))
-                x = x + 1
-
-            tags = []
-            for sp in sentencas_processadas:
-                for s in sp:
-                    tags.append(s.retorna_estrutura())
-        
-            bem_formada = utils.verifica_C1_cenario(sentencas_processadas)
-            atomica = utils.verifica_C2_cenario(sentencas_processadas)
-            minima = utils.verifica_C3_cenario(sentencas_processadas, bem_formada)
-
-            precondicao = utils.extrair_preCondicao(sentencas_processadas[0])    
-            acao = utils.extrair_acao(sentencas_processadas[1])     
-            finalidade = utils.extrair_finalidade(sentencas_processadas[2]) 
-            erros = utils.verifica_erros_cenario(bem_formada, atomica, minima, utils.valida_precondicao_cenario(sentencas_processadas[0]), utils.retorna_acao_cenario(sentencas_processadas[1]), utils.retorna_finalidade_cenario(sentencas_processadas[2]))        
-            end = timeit.default_timer()  
-            tempo = utils.formatar_tempo(start, end)
-
-            if erros == None:
-                return ResponseCenario(cenario, Constantes.NLTK, tempo, bem_formada, atomica, minima, precondicao, acao, finalidade, tags, erros)
-            else:
-                return ResponseCenario(cenario, Constantes.NLTK, tempo, bem_formada, atomica, minima, None, None, None, tags, erros)
-        else:
-            end = timeit.default_timer()  
-            tempo = utils.formatar_tempo(start, end)
-            return utils.retorna_erro_historia(cenario, Constantes.NLTK, sentencas, tempo)
-    
-    
+class NLTKService(): 
+       
     def tokenizar(sentenca:str, idioma:str):   
         tokens_sentencas = []
         tokens_palavras = []
--- src/services/ProcessadorService.py
+++ src/services/ProcessadorService.py
@@ -1,11 +1,16 @@
+import timeit
 from src.services.NLTKService import NLTKService
 from src.services.SpacyService import SpacyService
+from src.services.UtilsService import UtilsService
+from src.classes.ResponseHistoria import ResponseHistoria
+from src.classes.ResponseCenario import ResponseCenario
+from src.classes import Constantes
 
 class ProcessadorService():
     
     def __init__(self):
         pass   
-     
+         
     
     def processar(self, payload, type):
         
@@ -16,14 +21,107 @@ class ProcessadorService():
             
             if type == 'historia':
                 historia = p.get('historia')
-                response.append(NLTKService.processarHistoria(idioma, historia))
-                response.append(SpacyService.processarHistoria(idioma, historia))
+                response.append(ProcessadorService.processarHistoria(idioma, historia, Constantes.NLTK))
+                response.append(ProcessadorService.processarHistoria(idioma, historia, Constantes.SPACY))
             elif type == 'cenario':
                 cenario = p.get('cenario')
-                response.append(NLTKService.processarCenario(idioma, cenario))
-                response.append(SpacyService.processarCenario(idioma, cenario))
+                response.append(ProcessadorService.processarCenario(idioma, cenario, Constantes.NLTK))
+                response.append(ProcessadorService.processarCenario(idioma, cenario, Constantes.SPACY))
                 
         return response
     
+    
+    def processarHistoria(idioma:str, historia:str, tecnologia:str):
+        start = timeit.default_timer()
+        finalidade = None
+        erros = None
+
+        sentencas = utils.separar_sentencas_historia(historia)
+        
+        sentencas_processadas = []
+        
+        if len(sentencas) > 1 and not utils.verifica_erro_separacao(sentencas):
+            x = 0
+
+            while x < len(sentencas):
+                if tecnologia == Constantes.NLTK:
+                    sentencas_processadas.append(NLTKService.processar(sentencas[x], idioma))
+                if tecnologia == Constantes.SPACY:
+                    sentencas_processadas.append(SpacyService.processar(sentencas[x], idioma))
+                x = x + 1
+
+            tags = []
+            for sp in sentencas_processadas:
+                for s in sp:
+                    tags.append(s.retorna_estrutura())
+
+            bem_formada = utils.verifica_C1_historia(sentencas_processadas)
+            atomica = utils.verifica_C2_historia(sentencas_processadas)
+            minima = utils.verifica_C3_historia(sentencas_processadas, bem_formada)
+            ator = utils.extrair_ator(sentencas_processadas[0])    
+            acao = utils.extrair_acao(sentencas_processadas[1])
+
+            if len(sentencas) > 2:      
+                finalidade = utils.extrair_finalidade(sentencas_processadas[2])  
+                erros = utils.verifica_erros_historia(bem_formada, atomica, minima, utils.valida_ator_historia(sentencas_processadas[0]), utils.valida_acao_historia(sentencas_processadas[1]), utils.valida_finalidade_historia(sentencas_processadas[2]))
+            else:
+                erros = utils.verifica_erros_historia(bem_formada, atomica, minima, utils.valida_ator_historia(sentencas_processadas[0]), utils.valida_acao_historia(sentencas_processadas[1]), None)
+                
+            end = timeit.default_timer()  
+            tempo = utils.formatar_tempo(start, end)
+
+            if erros == None:
+                return ResponseHistoria(historia, tecnologia, tempo, bem_formada, atomica, minima, ator, acao, finalidade, tags, erros)
+            else:
+                return ResponseHistoria(historia, tecnologia, tempo, bem_formada, atomica, minima, None, None, None, tags, erros)
+        else:
+            end = timeit.default_timer()  
+            tempo = utils.formatar_tempo(start, end)
+            return utils.retorna_erro_historia(historia, tecnologia, sentencas, tempo)
+        
+    
+    def processarCenario(idioma:str, cenario:str, tecnologia:str):
+        start = timeit.default_timer()
+
+        sentencas = utils.separar_sentencas_cenario(cenario)
+        
+        sentencas_processadas = []
+
+        if len(sentencas) > 1 and not utils.verifica_erro_separacao(sentencas):
+            x = 0
+
+            while x < len(sentencas):
+                if tecnologia == Constantes.NLTK:
+                    sentencas_processadas.append(NLTKService.processar(sentencas[x], idioma))
+                if tecnologia == Constantes.SPACY:
+                    sentencas_processadas.append(SpacyService.processar(sentencas[x], idioma))
+                x = x + 1
+
+            tags = []
+            for sp in sentencas_processadas:
+                for s in sp:
+                    tags.append(s.retorna_estrutura())
+        
+            bem_formada = utils.verifica_C1_cenario(sentencas_processadas)
+            atomica = utils.verifica_C2_cenario(sentencas_processadas)
+            minima = utils.verifica_C3_cenario(sentencas_processadas, bem_formada)
+
+            precondicao = utils.extrair_preCondicao(sentencas_processadas[0])    
+            acao = utils.extrair_acao(sentencas_processadas[1])     
+            finalidade = utils.extrair_finalidade(sentencas_processadas[2]) 
+            erros = utils.verifica_erros_cenario(bem_formada, atomica, minima, utils.valida_precondicao_cenario(sentencas_processadas[0]), utils.retorna_acao_cenario(sentencas_processadas[1]), utils.retorna_finalidade_cenario(sentencas_processadas[2]))        
+            end = timeit.default_timer()  
+            tempo = utils.formatar_tempo(start, end)
+
+            if erros == None:
+                return ResponseCenario(cenario, tecnologia, tempo, bem_formada, atomica, minima, precondicao, acao, finalidade, tags, erros)
+            else:
+                return ResponseCenario(cenario, tecnologia, tempo, bem_formada, atomica, minima, None, None, None, tags, erros)
+        else:
+            end = timeit.default_timer()  
+            tempo = utils.formatar_tempo(start, end)
+            return utils.retorna_erro_historia(cenario, tecnologia, sentencas, tempo)    
+    
 
 processador = ProcessadorService()
+utils = UtilsService()
